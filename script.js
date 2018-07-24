3d462e9383667f222fdb0f4a43e9ed4c100b7ffb
DePaul <CONF0578@depaul.edu>
2018-07-24 15:45:48 -0500

    commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	$("button").click(function(){
+	$("h1").css("text-align", "right");
+	$("h1").css("color", "red");
+	var sentence = "The" +  numOfpeps  + "people ate" + numOfcandy  +  "pieces of candy. So each person get" +  b  + "piece of a candy.";
+console.log(sentence);
+	var name = $("#first_name").val();
+console.log(name);
+	});
+});
+
+var numOfpeps = 200;
+var numOfcandy = 40;
+var b = 0
+console.log(numOfpeps);
+console.log(numOfcandy);
+console.log(b);
+b = numOfpeps / numOfcandy
+
+var sentence = "The" +  numOfpeps  + "people ate" + numOfcandy  +  "pieces of candy. So each person get" +  b  + "piece of a candy.";
+console.log(sentence);
+var name = $("")
+
+var name = $("#first_name").val();
+console.log(name);
+
+
+
+
+
+
+
